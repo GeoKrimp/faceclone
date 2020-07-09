03d9b60fbc1abe971ba4503bd68aed43387404e3
GeoKrimp <48586785+GeoKrimp@users.noreply.github.com>
2020-07-09 18:32:05 +0300

    Add files via upload

---
+++ precache-manifest.25b82008aec563669cd9bfbf3b4621d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9cb4dc3165b2d58e0f65260ffb19018",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d15d1732f84de330374",
+    "url": "/static/css/main.8bb6746d.chunk.css"
+  },
+  {
+    "revision": "d84d6e585658fa1c972b",
+    "url": "/static/js/2.24244e4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.24244e4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d15d1732f84de330374",
+    "url": "/static/js/main.bb9f495f.chunk.js"
+  },
+  {
+    "revision": "f6246dd9f149776a1857",
+    "url": "/static/js/runtime-main.dc92daa8.js"
+  },
+  {
+    "revision": "5411bbeac18e02661efd3eadefb7da77",
+    "url": "/static/media/header.5411bbea.png"
+  }
+]);
